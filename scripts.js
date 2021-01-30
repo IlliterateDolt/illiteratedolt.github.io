a790a5d95bd749b056f6e0833540b0f01e92e9d0
IlliterateDolt <68133548+IlliterateDolt@users.noreply.github.com>
2021-01-29 19:30:01 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   $(".header-menu").click(function(){
-    $( "#menu-open" ).slideToggle( "slow" , "linear");
+    $( "#menu-open" ).show("slide", { direction: "left" }, 1000);
     console.log('The Button was Clicked');
    //debug alert("The button was clicked.");
   });
