dca1181ec9de1a09f594a3e56bc2b9b7f9fa0bad
IlliterateDolt <68133548+IlliterateDolt@users.noreply.github.com>
2021-01-29 22:38:34 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -6,4 +6,41 @@ $(document).ready(function(){
   });
 });
 
+<!--GSAP Animation Scripts --> 
+var $circle = $('.circle'),
+    $follow = $('.circle-follow');
 
+function moveCircle(e) {
+  TweenLite.to($circle, 0.3, {
+      x: e.clientX,
+      y: e.clientY
+  });
+  TweenLite.to($follow, 0.7, {
+      x: e.clientX,
+      y: e.clientY
+  });  
+}
+
+function hoverFunc(e) {
+  TweenLite.to($circle, 0.3, {
+    opacity: 1,
+    scale: 0
+  });
+  TweenLite.to($follow, 0.3, {
+      scale: 3
+  });  
+}
+
+function unhoverFunc(e) {
+  TweenLite.to($circle, 0.3, {
+    opacity: 1,
+    scale: 1
+  });
+  TweenLite.to($follow, 0.3, {
+      scale: 1
+  });  
+}
+
+$(window).on('mousemove', moveCircle);
+
+$("a").hover(hoverFunc, unhoverFunc);
