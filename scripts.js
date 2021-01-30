54e192fe915e3a1d3a655d0c315cf2ed7ed31662
IlliterateDolt <68133548+IlliterateDolt@users.noreply.github.com>
2021-01-29 22:47:58 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -6,41 +6,41 @@ $(document).ready(function(){
   });
 });
 
-<!--GSAP Animation Scripts --> 
-var $circle = $('.circle'),
-    $follow = $('.circle-follow');
 
-function moveCircle(e) {
-  TweenLite.to($circle, 0.3, {
-      x: e.clientX,
-      y: e.clientY
+$(document).ready(function(){
+  
+  var mousePos = {};
+
+ function getRandomInt(min, max) {
+   return Math.round(Math.random() * (max - min + 1)) + min;
+ }
+  
+  $(window).mousemove(function(e) {
+    mousePos.x = e.pageX;
+    mousePos.y = e.pageY;
   });
-  TweenLite.to($follow, 0.7, {
-      x: e.clientX,
-      y: e.clientY
-  });  
-}
-
-function hoverFunc(e) {
-  TweenLite.to($circle, 0.3, {
-    opacity: 1,
-    scale: 0
+  
+  $(window).mouseleave(function(e) {
+    mousePos.x = -1;
+    mousePos.y = -1;
   });
-  TweenLite.to($follow, 0.3, {
-      scale: 3
-  });  
-}
+  
+  var draw = setInterval(function(){
+    if(mousePos.x > 0 && mousePos.y > 0){
+      
+      var range = 15;
+      
+      var color = "background: rgb("+getRandomInt(0,255)+","+getRandomInt(0,255)+","+getRandomInt(0,255)+");";
+      
+      var sizeInt = getRandomInt(10, 30);
+      size = "height: " + sizeInt + "px; width: " + sizeInt + "px;";
+      
+      var left = "left: " + getRandomInt(mousePos.x-range-sizeInt, mousePos.x+range) + "px;";
+      
+      var top = "top: " + getRandomInt(mousePos.y-range-sizeInt, mousePos.y+range) + "px;"; 
 
-function unhoverFunc(e) {
-  TweenLite.to($circle, 0.3, {
-    opacity: 1,
-    scale: 1
-  });
-  TweenLite.to($follow, 0.3, {
-      scale: 1
-  });  
-}
-
-$(window).on('mousemove', moveCircle);
-
-$("a").hover(hoverFunc, unhoverFunc);
+      var style = left+top+color+size;
+      $("<div class='ball' style='" + style + "'></div>").appendTo('#wrap').one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend", function(){$(this).remove();}); 
+    }
+  }, 1);
+});
